e9346932ec739f4d317b82cb9e73fe948d27a0eb
SenhorDosSonhos1 <marcelosamu2002@gmail.com>
2026-03-24 08:23:04 -0300

    create custom user model with email authentication

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+#Custom User model imports
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
