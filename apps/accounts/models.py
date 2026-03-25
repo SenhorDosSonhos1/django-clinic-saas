840840435b2e03da58879d9d3c5709414669b197
SenhorDosSonhos1 <marcelosamu2002@gmail.com>
2026-03-25 07:53:03 -0300

    feat: add role field with choices to custom user model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -4,8 +4,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('staff', 'Staff'),
+        ('doctor', 'Doctor'),
+        ('admin', 'Admin'),
+    )
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='admin')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
